e3a9896c5871b34ab457036031ffb69505030ebb
Ami Patel <ahpatel@andrew.cmu.edu>
2023-03-23 14:38:18 -0700

    addressing the comment

--- backend/config/run.sh
+++ backend/config/run.sh
@@ -1,1 +1,3 @@
-uvicorn main:user_management_app --reload --host 0.0.0.0 --port 5000 & uvicorn main:room_reservation_app --reload --host 0.0.0.0 --port 8000 & uvicorn main:event_app --reload --host 0.0.0.0 --port 8080 & uvicorn main:admin_app --reload --host 0.0.0.0 --port 9000
+source ./config/.env
+
+uvicorn main:user_management_app --reload --host 0.0.0.0 --port $USER_MANAGEMENT_SERVICE_PORT & uvicorn main:room_reservation_app --reload --host 0.0.0.0 --port $ROOM_RESERVATION_SERVICE_PORT & uvicorn main:event_app --reload --host 0.0.0.0 --port $EVENT_SERVICE_PORT & uvicorn main:admin_app --reload --host 0.0.0.0 --port $ADMIN_SERVICE_PORT
