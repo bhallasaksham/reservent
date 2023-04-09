1e756008e1ebe4a675f7cf75065e174297e732ee
yixinsun <121929989@qq.com>
2023-04-08 17:54:21 -0700

    feat: add guests

--- front-end/src/pages/addEvent/AddEventPage.jsx
+++ front-end/src/pages/addEvent/AddEventPage.jsx
@@ -6,7 +6,11 @@ import styles from "./AddEventPage.module.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { RoomCard, CustomBadge } from "../../components";
-import { PlusSquare, PencilSquare } from "react-bootstrap-icons";
+import {
+  PlusSquare,
+  PencilSquare,
+  PersonPlusFill,
+} from "react-bootstrap-icons";
 
 export const AddEventPage = () => {
   const getRoundedDate = (date) => {
@@ -30,6 +34,8 @@ export const AddEventPage = () => {
   const [numOfParticipant, setNumOfParticipant] = useState();
   const [showSidebar, setShowSidebar] = useState(false);
   const [selectedRoom, setSelectedRoom] = useState();
+  const [curGuest, setCurGuest] = useState("");
+  const [guestList, setGuestList] = useState([]);
 
   const searchRooms = () => {
     setShowSidebar(true);
@@ -49,6 +55,25 @@ export const AddEventPage = () => {
 
   const fakeRoom = { id: 1, name: "room 118", size: 6 };
 
+  const addGuest = (guest) => {
+    if (curGuest != "") {
+      // TODO: check guest here
+      if (guestList.includes(guest)) {
+        alert(`You have already invited ${guest}`);
+      } else {
+        guestList.push(guest);
+        setGuestList(guestList);
+      }
+      setCurGuest("");
+    }
+    console.log(guestList);
+  };
+
+  const deleteGuest = (guest) => {
+    setGuestList(guestList.filter((item) => item !== guest));
+    console.log(guestList);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault(); // the default action that belongs to the event will not occur - whether we need this?
 
@@ -65,8 +90,12 @@ export const AddEventPage = () => {
         startTime +
         "\netime: " +
         endTime +
-        "\nnum: " +
-        numOfParticipant
+        "\nnum of p: " +
+        numOfParticipant +
+        "\ninvite guests: " +
+        guestList +
+        "\nroom: " +
+        selectedRoom.name
     );
   };
 
@@ -161,52 +190,77 @@ export const AddEventPage = () => {
                 onChange={(e) => setNumOfParticipant(e.target.value)}
               />
             </Form.Group>
-          </Col>
-          <Col></Col>
-        </Row>
 
-        <Form.Group className="mb-3" controlId="formRoom">
-          <Form.Label>Room</Form.Label>
-          <div>
-            {selectedRoom != null && (
-              <div className={styles["room-badge-wrapper"]}>
-                <CustomBadge
-                  content={selectedRoom.name}
-                  deleteContent={() => deleteRoom(selectedRoom)}
-                />
+            <Form.Group className="mb-3" controlId="formRoom">
+              <Form.Label>Room</Form.Label>
+              <div>
+                {selectedRoom != null && (
+                  <div className={styles["room-badge-group"]}>
+                    <CustomBadge
+                      content={selectedRoom.name}
+                      deleteContent={() => deleteRoom(selectedRoom)}
+                    />
+                  </div>
+                )}
+                {selectedRoom == null && (
+                  <PlusSquare
+                    className={styles["search-room-button"]}
+                    onClick={searchRooms}
+                  />
+                )}
+                {selectedRoom != null && (
+                  <PencilSquare
+                    className={styles["search-room-button"]}
+                    onClick={searchRooms}
+                  />
+                )}
+
+                <Offcanvas
+                  show={showSidebar}
+                  placement={"end"}
+                  onHide={() => setShowSidebar(false)}
+                >
+                  <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Available Rooms</Offcanvas.Title>
+                  </Offcanvas.Header>
+                  <Offcanvas.Body>
+                    <RoomCard
+                      room={fakeRoom}
+                      chooseRoom={() => chooseRoom(fakeRoom)}
+                    />
+                  </Offcanvas.Body>
+                </Offcanvas>
               </div>
-            )}
-            {selectedRoom == null && (
-              <PlusSquare
-                className={styles["search-room-button"]}
-                onClick={searchRooms}
-              />
-            )}
-
-            {selectedRoom != null && (
-              <PencilSquare
-                className={styles["search-room-button"]}
-                onClick={searchRooms}
-              />
-            )}
-
-            <Offcanvas
-              show={showSidebar}
-              placement={"end"}
-              onHide={() => setShowSidebar(false)}
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Available Rooms</Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <RoomCard
-                  room={fakeRoom}
-                  chooseRoom={() => chooseRoom(fakeRoom)}
-                />
-              </Offcanvas.Body>
-            </Offcanvas>
-          </div>
-        </Form.Group>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formInviteGuests">
+              <Form.Label>Invite Guests</Form.Label>
+              <div>
+                <div className={styles["input-guest-wrapper"]}>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email address"
+                    value={curGuest}
+                    onChange={(e) => setCurGuest(e.target.value)}
+                  />
+                  <PersonPlusFill
+                    className={styles["add-guest-button"]}
+                    onClick={() => addGuest(curGuest)}
+                  />
+                </div>
+              </div>
+              <div className={styles["guest-badge-group"]}>
+                {guestList.map((guest) => (
+                  <CustomBadge
+                    content={guest}
+                    deleteContent={() => deleteGuest(guest)}
+                  />
+                ))}
+              </div>
+            </Form.Group>
+          </Col>
+        </Row>
 
         <Row>
           <Col>
