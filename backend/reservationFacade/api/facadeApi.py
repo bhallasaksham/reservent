963c3e7df0ad1f70f074f9f94e782a340f3068d4
Saksham Bhalla <bhallasaksham@gmail.com>
2023-04-11 13:27:34 -0700

    fix: update facadeApi after get_available_rooms implementation

--- backend/reservationFacade/api/facadeApi.py
+++ backend/reservationFacade/api/facadeApi.py
@@ -1,7 +1,10 @@
 from typing import Optional
-from fastapi import APIRouter,FastAPI, Request, Response, HTTPException, Depends, Header, Form, Cookie, Query, status
+from fastapi import APIRouter, Response, HTTPException, Header, Cookie, Request, Body
+
+from reservationFacade.handler.facadeHandler import facade
 
 facadeRoutes = APIRouter()
+
 
 @facadeRoutes.get("/")
 async def root():
@@ -9,22 +12,21 @@ async def root():
 
 
 @facadeRoutes.get("/rooms/available")
-async def getAvailableRooms(response: Response, token: Optional[str] = Header(None)):
+async def get_available_rooms(request: Request):
     try:
-        return facade(url='http://127.0.0.1:8000/rooms/available', http_verb='GET', token=token)
+        return facade(url='http://127.0.0.1:8000/rooms/available', http_verb='GET', request=request)
     except HTTPException as e:
-        response.status_code = e.status_code
         return {"message": e.detail}
 
+
 @facadeRoutes.post("/rooms/:room/reserve")
-async def reserveRoom(response: Response, token: Optional[str] = Header(None)):
+async def reserve_room(request: Request):
     try:
-        createEventResponse = facade(url="http://127.0.0.1:8080/events/create", http_verb='POST', token=token)
+        createEventResponse = facade(url="http://127.0.0.1:8080/events/create", http_verb='POST', request=request)
         if createEventResponse is not None:
-            roomReservationResponse = facade(url="http://127.0.0.1:8000/rooms/reserve", http_verb='POST', token=token)
+            roomReservationResponse = facade(url="http://127.0.0.1:8000/rooms/reserve", http_verb='POST',  request=request)
             if roomReservationResponse is not None:
-                return facade(url="http://127.0.0.1:8080/events/finalize", http_verb='PUT', token=token);
+                return facade(url="http://127.0.0.1:8080/events/finalize", http_verb='PUT',  request=request);
     except HTTPException as e:
-        response.status_code = e.status_code
         return {"message": e.detail}
 
