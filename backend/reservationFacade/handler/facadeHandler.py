fed9f861bc4b2319f68cbc76b4dbdf790db3a7e7
Saksham Bhalla <bhallasaksham@gmail.com>
2023-04-13 09:29:52 -0700

    fix: return same json response as called service in facade layer

--- backend/reservationFacade/handler/facadeHandler.py
+++ backend/reservationFacade/handler/facadeHandler.py
@@ -1,6 +1,7 @@
 import os
 import jwt
 from fastapi import HTTPException, status, Request
+from fastapi.responses import JSONResponse
 import requests
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or None
@@ -14,7 +15,7 @@ def decode_jwt(encoded_token):
 
 
 async def facade(url: str, http_verb: str, request: Request):
-    if request.cookies is None:
+    if request.headers is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
     jwt_token = request.headers["authorization"].split(" ")[1]
     decoded_token = decode_jwt(jwt_token)
@@ -30,16 +31,17 @@ async def facade(url: str, http_verb: str, request: Request):
         for key, value in params.items():
             data[key] = value
 
-    if http_verb == 'GET':
-        response = requests.get(url, headers=headers, json=data)
-    elif http_verb == 'PUT':
-        response = requests.put(url, headers=headers, json=data)
-    elif http_verb == 'POST':
-        response = requests.post(url, headers=headers, json=data)
-    elif http_verb == 'DELETE':
-        response = requests.delete(url, headers=headers, json=data)
-    else:
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            detail="Invalid HTTP Verb in Facade Layer")
 
-    return response.json()
+    if http_verb == 'GET':
+            response = requests.get(url, headers=headers, json=data)
+        elif http_verb == 'PUT':
+            response = requests.put(url, headers=headers, json=data)
+        elif http_verb == 'POST':
+            response = requests.post(url, headers=headers, json=data)
+        elif http_verb == 'DELETE':
+            response = requests.delete(url, headers=headers, json=data)
+        else:
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                                detail="Invalid HTTP Verb in Facade Layer")
+
+    return JSONResponse(status_code=response.status_code, content=response.content)
