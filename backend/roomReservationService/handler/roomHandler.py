046b85dca3a1d18e29691d43d5d84f23ba98fb0c
shuyucS2023 <shuyuc@andrew.cmu.edu>
2023-04-20 17:35:43 -0700

    implement decorator to get available rooms api

--- backend/roomReservationService/handler/roomHandler.py
+++ backend/roomReservationService/handler/roomHandler.py
@@ -3,6 +3,7 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from ..dao import RoomDao
 from ..utils.datetime import get_datetime_from_time_string, get_time_in_google_api_compatible_format
+from database.schemas.roomSchema import Privilege
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 TIMEZONE = 'America/Los_Angeles'
@@ -12,11 +13,15 @@ if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
     raise BaseException('Missing env variables')
 
 
+class GetRoomsInterface:
+    def get_rooms(self):
+        pass
+
+
 class GetRoomsHandler:
 
     def __init__(self, request):
-        RoomDao().initRooms()
-        self.all_rooms = RoomDao().getRooms()
+        self.roomDao = RoomDao()
         self.available_rooms = []
         self.request = request
 
@@ -39,20 +44,59 @@ class GetRoomsHandler:
         event_start_dt = get_datetime_from_time_string(event['start']['dateTime'])
         room_availability_timediff = event_start_dt - start_dt
         room_requested_timediff = end_dt - start_dt
-        if room_availability_timediff > room_requested_timediff and room.size >= int(self.request.num_guests):
+        if room_availability_timediff > room_requested_timediff and room.capacity >= int(self.request.num_guests):
             return True
         return False
 
     def get_rooms(self):
         self.request.start_time = get_time_in_google_api_compatible_format(self.request.start_time)
         self.request.end_time = get_time_in_google_api_compatible_format(self.request.end_time)
-        for room in self.all_rooms:
+        for room in self.roomDao.getRoomsByPrivilege(privilege=Privilege.STUDENT):
             calendar_id = room.url.split('=')[1]
             events_list = self.get_events_from_google_calendar(calendar_id)
             for event in events_list:
                 if self.is_room_available(room, event):
-                    self.available_rooms.append(room.getRoom())
+                    self.available_rooms.append({
+                        'name': room.name,
+                        'capacity': room.capacity,
+                    })
         return self.available_rooms
+    
+class GetRoomsDecorator(GetRoomsInterface):
+    _interface : GetRoomsInterface = None
+
+    def __init__(self, interface):
+        self._interface = interface
+    
+    def __getattr__(self, name):
+        """
+        Delegate all unimplemented methods to the wrapped component.
+        """
+        return getattr(self._interface, name)
+
+    def get_rooms(self):
+        return self._interface.getRooms()
+
+
+class GetRoomsDecoratorAdmin(GetRoomsDecorator):
+    def __init__(self, interface, request):
+        super().__init__(interface)
+        self.request = request
+        self.roomDao = RoomDao()
+
+    def get_rooms(self):
+        rooms = self._interface.get_rooms()
+        for room in self.roomDao.getRoomsByPrivilege(privilege=Privilege.STAFF_AND_FACULTY):
+            calendar_id = room.url.split('=')[1]
+            events_list = self.get_events_from_google_calendar(calendar_id)
+            for event in events_list:
+                if self.is_room_available(room, event):
+                    rooms.append({
+                        'name': room.name,
+                        'capacity': room.capacity,
+                    })
+        return rooms
+
 
 
 class ReserveRoomHandler:
